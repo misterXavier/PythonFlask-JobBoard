c48ef41dcf42b824149e8613d88af85c47262bf0
Rodrigo Felippe <rfelippe@allstate.ca>
2019-08-30 14:44:14 -0400

    added sqlite conn

--- jobs/app.py
+++ jobs/app.py
@@ -6,7 +6,7 @@ PATH='db/jobs.sqlite'
 app = Flask(__name__)
 
 def open_connection():
-	connection = getatrr(g,'_connection',None)
+	connection = getattr(g,'_connection',None)
 	if connection == None:
 		connection = g._connection = sqlite3.connect(PATH)
 	connection.row_factory = sqlite3.Row
@@ -25,7 +25,7 @@ def execute_sql(sql,values=(),commit=False,single=False):
 
 @app.teardown_appcontext
 def close_connection(exception):
-	connection = getatrr(g,'_connection',None)
+	connection = getattr(g,'_connection',None)
 	if connection is not None:
 		connection.close()
 
